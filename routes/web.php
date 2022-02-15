efa410a3a9c3d4e3bf0efc8319434a0ffcacc30e
Marvycode <gwlad.marvy@gmail.com>
2022-02-15 10:01:08 +0100

    first commit

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,8 @@
 <?php
 
-use App\Http\Controllers\Admin\UserCtrl;
-use App\Http\Controllers\HomeCtrl;
 use App\Http\Livewire\Personnels;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
-
-
-// Route::get('/', function () {
-//     return view('pages.index');
-// })->name('index');
-
-// Route::get('/notre-equipe', [HomeCtrl::class, 'equipe'])->name('equipe');
-// Route::get('/contacts', [HomeCtrl::class, 'contacts'])->name('contacts');
-// Route::get('/articles-de-blog', [HomeCtrl::class, 'allArticles'])->name('allArticles');
-// Route::get('/c-est-quoi-un-site-vitrine', [HomeCtrl::class, 'siteVitrine'])->name('siteVitrine');
-// Route::get('/site-blog', [HomeCtrl::class, 'blog'])->name('blog');
-// Route::get('/site-e-commerce', [HomeCtrl::class, 'eCommerce'])->name('eCommerce');
 
 
 Auth::routes();
